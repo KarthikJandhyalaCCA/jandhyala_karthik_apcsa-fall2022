afa788c8f2bc97e1c6e20f505348584e7e214df3
KarthikJandhyalaCCA <111382987+KarthikJandhyalaCCA@users.noreply.github.com>
2022-08-25 11:18:00 -0700

    Add files via upload

---
+++ Unit-05/src/AvgLen.java
@@ -0,0 +1,15 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+public class AvgLen
+{
+   public static double go( String a, String b )
+	{
+	   double len1 = (double) a.length();
+	   double len2 = (double) b.length();
+	   double average = ((len1+len2)/2);
+	   return average;
+	}
+}
