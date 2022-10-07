73d42eec25738a28991cb0603b59b556d67a5eb0
KarthikJandhyalaCCA <111382987+KarthikJandhyalaCCA@users.noreply.github.com>
2022-10-07 11:38:43 -0700

    Add files via upload

--- Unit-11/src/ToyRunner.java
+++ Unit-11/src/ToyRunner.java
@@ -4,9 +4,25 @@
 
 import static java.lang.System.*;
 
+import java.util.Scanner;
+
 public class ToyRunner
 {
 	public static void main(String[] args)
 	{
+		Scanner keyboard = new Scanner(System.in);
+		Toy t = new Toy();
+	   	String choice="";
+		out.print("\nEnter a toy name :: ");
+		String word = keyboard.next();
+		//call the printWord method
+		t.setName(word);
+		
+		out.print("\nEnter a toy count :: ");
+		int count = keyboard.nextInt();
+		//call the printWord method
+		t.setCount(count);
+		
+		out.println(t.toString());
 	}
 }
--- Unit-11/src/ToyStore.java
+++ Unit-11/src/ToyStore.java
@@ -14,24 +14,45 @@ public class ToyStore
 
 	public ToyStore()
 	{
+		loadToys("");
 	}
 
 	public void loadToys( String toys )
 	{
+		Toy t = new Toy(toys);
+		toyList.add(t);
 	}
   
   	public Toy getThatToy( String nm )
   	{
+  		for (int i =0; i<toyList.size(); i++) {
+  			if (toyList.get(i).getName() == nm) {
+  				return toyList.get(i);
+  			}
+  		}
   		return null;
   	}
   
   	public String getMostFrequentToy()
   	{
-  		return "";
+  		int check = toyList.get(0).getCount();
+  		int j = 0;
+  		for (int i = 0; i<toyList.size(); i++) {
+  			if ((toyList.get(i).getCount())>check) {
+  				check = toyList.get(i).getCount();
+  				j = i;
+  			}
+  		}
+  		
+  		return toyList.get(j).getName();
   	}  
   
   	public void sortToysByCount()
   	{
+  		int[] arr = new int[toyList.size()];
+  		for (int i = 0; i<toyList.size(); i++) {
+  			
+  		}
   	}  
   	  
 	public String toString()
