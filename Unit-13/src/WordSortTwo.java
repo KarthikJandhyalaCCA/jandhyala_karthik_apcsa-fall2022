87c13a3cdbdc73f18bc73fae439503d18c2b393c
KarthikJandhyalaCCA <111382987+KarthikJandhyalaCCA@users.noreply.github.com>
2022-11-03 11:20:42 -0700

    Add files via upload

--- Unit-13/src/WordSortTwo.java
+++ Unit-13/src/WordSortTwo.java
@@ -12,15 +12,33 @@ public class WordSortTwo
 
 	public WordSortTwo(String sentence)
 	{
+		wordRay = sentence.split(" ");
 	}
 
 	public void sort()
 	{
+		for (int i =0; i<wordRay.length-1; i++) {
+			int min = i;
+			for (int j = i+1; j<wordRay.length; j++) {
+				if (wordRay[j].compareTo(wordRay[min]) > 0) {
+					min = j;
+				}
+			}
+			if (min != i) {
+				String temp = wordRay[min];
+				wordRay[min] = wordRay[i];
+				wordRay[i] = temp;
+			}
+		}
 	}
 
 	public String toString()
 	{
 		String output="";
+		sort();
+		for (int i = wordRay.length-1; i>=0; i--) {
+			output += wordRay[i] + "\n";
+		}
 		return output+"\n\n";
 	}
 }
